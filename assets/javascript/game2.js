65dcb5556d75ecae25c4ac6975b1b07b6efa51fc
4cody <cshaner04@gmail.com>
2017-08-12 23:43:35 -0700

    gamestate as is to be turned in

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -18,7 +18,7 @@ var jengo = {
 var wampa = {
 	attack:20,
 	cAttack:20,
-	hp:150,
+	hp:50,
 	name:"Wampa"
 };
 var trooper = {
@@ -30,15 +30,22 @@ var trooper = {
 
 $(document).ready(function() {
 
+	$("#restart").click(function() {
+		newGame();
+	});
+
 	$("#yoda").click(function() {
 		if(pow){
 			$(this).removeClass("fighter");
 			draw();
 			user = yoda;
+			$("#line1").html("Pick an enemy");
+			stat();
 		}
 		else{
 			$(this).appendTo("#opp").addClass("opponent");;
 			enemy = yoda;
+			stat();
 		}
 	});
 
@@ -47,10 +54,12 @@ $(document).ready(function() {
 			$(this).removeClass("fighter");
 			draw();
 			user = jengo;
+			$("#line1").html("Pick an enemy");
 		}
 		else{
 			$(this).appendTo("#opp").addClass("opponent");;
 			enemy = jengo;
+			stat();
 		}
 	});
 
@@ -59,10 +68,12 @@ $(document).ready(function() {
 			$(this).removeClass("fighter");
 			draw();
 			user = trooper;
+			$("#line1").html("Pick an enemy");
 		}
 		else{
 			$(this).appendTo("#opp").addClass("opponent");;
-			enemy = trooper;
+			enemy = trooper;	
+			stat();	
 		}
 	});
 
@@ -71,34 +82,38 @@ $(document).ready(function() {
 			$(this).removeClass("fighter");
 			draw();
 			user = wampa;
+			$("#line1").html("Pick an enemy");
 		}
 		else{
 			$(this).appendTo("#opp").addClass("opponent");
-			enemy = wampa;
+			enemy = wampa;				
+			stat();
 		}
 	});
 
+	if($('#enemies').children().length == 0 && $('#opp').children().length == 0){
 
+	};
+
+   //      combat  checks   
 	$("#attack").click(function() {
 		if(enemy.hp > 0 && user.hp > 0){
 			fightCheck();
+			stat();
 		}
-		if(enemy.hp <= 0){
+		if(enemy.hp < 0){
 			if($('#enemies').children().length == 0){
 				alert("You've vanquished all foes!");
 			}else{
+				fightCheck();
 				alert("You've slain an enemy!");
+				bye();
+				stat();	
 			}
-			bye();
 		}
-		if(user.hp <= 0){
-
-		}
+		
 	});
 
-	// if ($('#enemies').children().length == 0 && $("#opp").children().length == 0 &&){
-	// 	alert("yes");
-	// };
 
 });
 
@@ -109,17 +124,26 @@ function draw() {
 };
 
 function fightCheck(){
-	user.hp = user.hp - enemy.cAttack;
-	enemy.hp = enemy.hp - user.attack;
-	user.attack = user. attack * 1.7;
+	user.hp = Math.round(user.hp - enemy.cAttack);
+	enemy.hp = Math.round(enemy.hp - user.attack);
+	user.attack = Math.round(user. attack * 1.5);
 	console.log(user.hp);
 	console.log(enemy.hp);
 };
 
 function bye(){
 	$(".opponent").hide();
-}
+};
 
-function dead(){
+function stat(){
+	$("#user").empty();
+	$("#other").empty();
+	$("#user").append(user.name + "- HP:" + user.hp + '<br>' + user.name + "- AP:" + user.attack + '<br>');
+	$("#other").append(enemy.name + "- HP:" + enemy.hp + '<br>' + enemy.name + "- AP:" + enemy.attack);
+};
 
-}
+function newGame(){
+	gameStage = "draw";
+	pow = true;
+	$("restart").hide();
+};
